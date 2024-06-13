1948870cf09f935e4da24a252c0c016b895a0fdf
Neliz3 <eliza.nadrega14@gmail.com>
2024-06-13 18:58:38 +0300

    serializable added

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from read_uncommited import dirty_read
 from read_commited import non_repeatable_reads
 from repeatable_read import phantom_reads
+from serializable import serializable
 
 
 if __name__ == "__main__":
     # dirty_read()
     # non_repeatable_reads()
-    phantom_reads()
+    # phantom_reads()
+    serializable()
---
+++ serializable.py
@@ -0,0 +1,53 @@
+from mysql.connector import Error
+from datetime import datetime
+
+from configs import create_connection
+
+
+def serializable():
+    """
+    Shows how SERIALIZABLE isolation level works.
+    Solves previous issues.
+    """
+    cursor1 = None
+    cursor2 = None
+    connection1 = create_connection()
+    connection2 = create_connection()
+
+    try:
+        cursor1 = connection1.cursor()
+        cursor2 = connection2.cursor()
+
+        # Transaction 1 [BEFORE]: SERIALIZABLE
+        print(f"Transaction 1 started: {datetime.now()}")
+        connection1.start_transaction(isolation_level='REPEATABLE READ')
+        cursor1.execute("SELECT COUNT(*) FROM accounts WHERE name = 'Tom'")
+        count_serializable = cursor1.fetchone()[0]
+
+        print(f"SERIALIZABLE BEFORE: count of rows = {count_serializable}")
+
+        # Transaction 2: SERIALIZABLE
+        print(f"Transaction 2 started: {datetime.now()}")
+        connection2.start_transaction(isolation_level='REPEATABLE READ')
+        cursor2.execute("INSERT INTO accounts (name, balance) VALUES ('Tom', 1212)")
+        print(f"Transaction 2 commit(): {datetime.now()}")
+        connection2.commit()
+
+        # Transaction 1 [AFTER]: SERIALIZABLE
+        print(f"Transaction 1 continued: {datetime.now()}")
+        cursor1.execute("SELECT COUNT(*) FROM accounts WHERE name = 'Tom'")
+        count_serializable = cursor1.fetchone()[0]
+
+        print(f"SERIALIZABLE AFTER: count of rows = {count_serializable}")
+
+    except Error as e:
+        print(f"Error: {e}")
+    finally:
+        if cursor1:
+            cursor1.close()
+        if connection1 and connection1.is_connected():
+            connection1.close()
+        if cursor2:
+            cursor2.close()
+        if connection2 and connection2.is_connected():
+            connection2.close()
